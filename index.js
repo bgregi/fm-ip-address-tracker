e24d81ec34d45359db95119f6bacdd784d4b76e1
Bruno Geraldi <brunogeraldieng@gmail.com>
2021-09-21 22:33:30 -0300

    Working on maps API

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
+// import 
+
 const api_key = "at_rYoNfJQaPsngkRDwrhwT6RhscJWkU";
 
-$("button").on("click", function () {
-    let ip = $("#div-input").val()
-
+//GETS THE INFORMATION FROM IPIFY
+function getInfo(ipAddress, domainName) {
+    $("#ip").text("")
+    $("#location").text("")
+    $("#timezone").text("")
+    $("#isp").text("")
+    
     $.ajax({
         url: "https://geo.ipify.org/api/v1",
         data: {
             apiKey: api_key,
-            ipAddress: ip
+            ipAddress: ipAddress,
+            domain: domainName
         },
         success: function (data) {
-            // console.log("deu boa")
+            $("#ip").append(data.ip)
+            $("#location").append(`${data.location.city}, ${data.location.region} ${data.location.postalCode}`)
+            $("#timezone").append("UTC " + data.location.timezone)
+            $("#isp").append(data.isp)
 
-            let ipAddress = JSON.stringify(data, "", 2)
-            let ip2 = JSON.parse(ipAddress)
-
-            console.log(ipAddress)
-            console.log(typeof(ipAddress))
-            console.log(ip2)
-            console.log(typeof(ip2))
-            console.log("data is a " + typeof(data))
-            console.log(data)
-
-
-            // $("body").append("<pre>" + JSON.stringify(data, "", 2) + "</pre>");
             // console.log(data)
+        },
+        error: function () {
+            alert("Please type a correct IP address or domain name")
         }
     })
+}
+
+//GETS USER INFO AT PAGE LOAD
+getInfo("", "")
+
+//GETS INFO FROM USER INPUT
+$("button").on("click", function () {
+    let regExp = /[a-zA-Z]/g
+    let input = $("input").val()
+
+    let domainName = ""
+    let ipAddress = ""
+
+    regExp.test(input) ? domainName = input : ipAddress = input
+
+    getInfo(ipAddress, domainName)
 })
+
+
+var mymap = L.map('map').setView([51.505, -0.09], 13);
